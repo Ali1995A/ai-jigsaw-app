7b1b756d416a1fe7da24b536458429eccef48447
Ali1995A <liexpress@gmail.com>
2026-01-17 07:45:18 +0800

    Move new default images to front

--- src/defaultImages.ts
+++ src/defaultImages.ts
@@ -6,86 +6,6 @@ export type ImageOption = {
 
 export const DEFAULT_IMAGES: ImageOption[] = [
   {
-    id: 'pixar-girl-1',
-    src: 'https://ac35844.webp.li/liexpress_Pixar-style_four-year-old_Chinese_girl_sitting_on_the_3b54a551-7921-4ee4-ac4f-19bba982a2f2.png',
-    label: '女孩（皮克斯风）1',
-  },
-  {
-    id: 'pixar-girl-2',
-    src: 'https://ac35844.webp.li/liexpress_Pixar-style_four-year-old_Chinese_girl_sitting_on_the_2aaaf338-3b1f-46c2-9ea2-94085d94f849.png',
-    label: '女孩（皮克斯风）2',
-  },
-  {
-    id: 'wechat-20250419075853',
-    src: 'https://ac35844.webp.li/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20250419075853.jpg',
-    label: '微信图 20250419075853',
-  },
-  {
-    id: '675d7939',
-    src: 'https://ac35844.webp.li/675d7939-4527-4d37-98fe-c853439478c4.png',
-    label: '插画 675d7939',
-  },
-  {
-    id: 'dc7900a1',
-    src: 'https://ac35844.webp.li/dc7900a1-9ff8-4cf1-9ff0-621b191e41d3.png',
-    label: '插画 dc7900a1',
-  },
-  {
-    id: 'a0cc0caa',
-    src: 'https://ac35844.webp.li/a0cc0caa-72ea-4b07-9537-d927320a2d5a.png',
-    label: '插画 a0cc0caa',
-  },
-  {
-    id: 'wechat-20250413231248',
-    src: 'https://ac35844.webp.li/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20250413231248.png',
-    label: '微信图 20250413231248',
-  },
-  {
-    id: 'mother-and-girl',
-    src: 'https://ac35844.webp.li/liexpress_A_30-year-old_Chinese_intellectual_mother_and_her_3-y_180a4d76-b6fd-439f-8b12-fe8263ec79b3.png',
-    label: '妈妈和女孩',
-  },
-  {
-    id: 'gf9t-xdlb',
-    src: 'https://ac35844.webp.li/Gf9tXdlbYAA50L1.jfif',
-    label: '照片 1',
-  },
-  {
-    id: 'gf9f-zdnak',
-    src: 'https://ac35844.webp.li/Gf9fZDnakAATyke.jfif',
-    label: '照片 2',
-  },
-  {
-    id: 'gf9f-yyr',
-    src: 'https://ac35844.webp.li/Gf9fYYRa0AAE4R4.jfif',
-    label: '照片 3',
-  },
-  {
-    id: 'gf9t-xdtb',
-    src: 'https://ac35844.webp.li/Gf9tXdtbAAAny_N.jfif',
-    label: '照片 4',
-  },
-  {
-    id: 'magic-girl',
-    src: 'https://ac35844.webp.li/liexpress_Magic_girl_wand_forest_rabbit_d298a05d-bf53-4137-895a-852c8fb246af.png',
-    label: '魔法少女',
-  },
-  {
-    id: 'girl-park',
-    src: 'https://ac35844.webp.li/liexpress_A_3-year-old_Chinese_girl_walking_outdoors_in_a_park__a0701788-b54a-483b-a00d-78b8c5a2fe95.png',
-    label: '公园散步',
-  },
-  {
-    id: 'girl-indoor-1',
-    src: 'https://ac35844.webp.li/liexpress_A_3-year-old_Chinese_girl_standing_indoors_in_front_o_835b94d6-c0a0-4192-819a-3cfb4c6d724d.png',
-    label: '室内 1',
-  },
-  {
-    id: 'girl-indoor-2',
-    src: 'https://ac35844.webp.li/liexpress_A_3-year-old_Chinese_girl_standing_indoors_in_front_o_0e4d3ddc-044c-49c6-9cdc-33c78fdecc21.png',
-    label: '室内 2',
-  },
-  {
     id: 'image-1',
     src: 'https://ac35844.webp.li/image%20(1).jpg',
     label: '图片 1',
@@ -169,5 +89,85 @@ export const DEFAULT_IMAGES: ImageOption[] = [
     id: 'image-16',
     src: 'https://ac35844.webp.li/image%20(16).jpg',
     label: '图片 16',
+  },
+  {
+    id: 'pixar-girl-1',
+    src: 'https://ac35844.webp.li/liexpress_Pixar-style_four-year-old_Chinese_girl_sitting_on_the_3b54a551-7921-4ee4-ac4f-19bba982a2f2.png',
+    label: '女孩（皮克斯风）1',
+  },
+  {
+    id: 'pixar-girl-2',
+    src: 'https://ac35844.webp.li/liexpress_Pixar-style_four-year-old_Chinese_girl_sitting_on_the_2aaaf338-3b1f-46c2-9ea2-94085d94f849.png',
+    label: '女孩（皮克斯风）2',
+  },
+  {
+    id: 'wechat-20250419075853',
+    src: 'https://ac35844.webp.li/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20250419075853.jpg',
+    label: '微信图 20250419075853',
+  },
+  {
+    id: '675d7939',
+    src: 'https://ac35844.webp.li/675d7939-4527-4d37-98fe-c853439478c4.png',
+    label: '插画 675d7939',
+  },
+  {
+    id: 'dc7900a1',
+    src: 'https://ac35844.webp.li/dc7900a1-9ff8-4cf1-9ff0-621b191e41d3.png',
+    label: '插画 dc7900a1',
+  },
+  {
+    id: 'a0cc0caa',
+    src: 'https://ac35844.webp.li/a0cc0caa-72ea-4b07-9537-d927320a2d5a.png',
+    label: '插画 a0cc0caa',
+  },
+  {
+    id: 'wechat-20250413231248',
+    src: 'https://ac35844.webp.li/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20250413231248.png',
+    label: '微信图 20250413231248',
+  },
+  {
+    id: 'mother-and-girl',
+    src: 'https://ac35844.webp.li/liexpress_A_30-year-old_Chinese_intellectual_mother_and_her_3-y_180a4d76-b6fd-439f-8b12-fe8263ec79b3.png',
+    label: '妈妈和女孩',
+  },
+  {
+    id: 'gf9t-xdlb',
+    src: 'https://ac35844.webp.li/Gf9tXdlbYAA50L1.jfif',
+    label: '照片 1',
+  },
+  {
+    id: 'gf9f-zdnak',
+    src: 'https://ac35844.webp.li/Gf9fZDnakAATyke.jfif',
+    label: '照片 2',
+  },
+  {
+    id: 'gf9f-yyr',
+    src: 'https://ac35844.webp.li/Gf9fYYRa0AAE4R4.jfif',
+    label: '照片 3',
+  },
+  {
+    id: 'gf9t-xdtb',
+    src: 'https://ac35844.webp.li/Gf9tXdtbAAAny_N.jfif',
+    label: '照片 4',
+  },
+  {
+    id: 'magic-girl',
+    src: 'https://ac35844.webp.li/liexpress_Magic_girl_wand_forest_rabbit_d298a05d-bf53-4137-895a-852c8fb246af.png',
+    label: '魔法少女',
+  },
+  {
+    id: 'girl-park',
+    src: 'https://ac35844.webp.li/liexpress_A_3-year-old_Chinese_girl_walking_outdoors_in_a_park__a0701788-b54a-483b-a00d-78b8c5a2fe95.png',
+    label: '公园散步',
+  },
+  {
+    id: 'girl-indoor-1',
+    src: 'https://ac35844.webp.li/liexpress_A_3-year-old_Chinese_girl_standing_indoors_in_front_o_835b94d6-c0a0-4192-819a-3cfb4c6d724d.png',
+    label: '室内 1',
+  },
+  {
+    id: 'girl-indoor-2',
+    src: 'https://ac35844.webp.li/liexpress_A_3-year-old_Chinese_girl_standing_indoors_in_front_o_0e4d3ddc-044c-49c6-9cdc-33c78fdecc21.png',
+    label: '室内 2',
   },
 ]
